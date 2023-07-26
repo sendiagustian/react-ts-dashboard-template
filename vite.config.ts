9e0faa867e8ba93b78e2902440df796b2ffe2d7e
sendiagustian <sendiagustian.pk@gmail.com>
2023-07-26 12:18:22 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/",
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        outDir: "dist",
+        chunkSizeWarningLimit: 1600,
+    },
+});
